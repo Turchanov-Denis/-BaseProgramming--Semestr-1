4c3e2551489e895c256427fc4494fe6097053d83
Turchanov Denis <turchanovdenis786@gmail.com>
2022-11-03 23:37:07 +0200

    complete
    had to shift indexes by 1

--- lab1/main.c
+++ lab1/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define Size 4
+#define Size 5
 
 int printArr(int arr[Size][Size])
 {
@@ -18,18 +18,18 @@ int main(void)
     int counter = 1;
     // fill full matrix
     // change: the matrix is imaginary extended, filling occurs only if the condition is met
-    for (int i = 0; i < Size * 2; i++)
+    for (int i = 1; i <= Size * 2; i++)
     {
         printf("%d\n", i);
         if (i % 2 == 0)
         {
             int m = i;
-            int n = 0;
-            while (m >= 0)
+            int n = 1;
+            while (m > 0)
             {
-                if ((m < Size) && (m >= 0) && (n < Size) && (n >= 0))
+                if ((m <= Size) && (m > 0) && (n <= Size) && (n > 0))
                 {
-                    arr[m][n] = counter;
+                    arr[m-1][n-1] = counter;
                     counter++;
                     printf("%d - %d\n", m + 1, n + 1);
                 }
@@ -40,14 +40,14 @@ int main(void)
         }
         else
         {
-            int m = 0;
+            int m = 1;
             int n = i;
-            while (n >= 0)
+            while (n > 0)
             {
 
-                if ((m < Size) && (m >= 0) && (n < Size) && (n >= 0))
+                if ((m <= Size) && (m > 0) && (n <= Size) && (n > 0))
                 {
-                    arr[m][n] = counter;
+                    arr[m-1][n-1] = counter;
                     counter++;
                     printf("%d - %d\n", m + 1, n + 1);
                 }
