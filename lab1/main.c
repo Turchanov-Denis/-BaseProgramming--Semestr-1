123d0a4cc7829eaddca0c949d0f1a1272d84c4d7
Turchanov Denis <turchanovdenis786@gmail.com>
2022-11-03 23:32:18 +0200

    fill full, but numbering is broken

--- lab1/main.c
+++ lab1/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define Size 5
+#define Size 4
 
 int printArr(int arr[Size][Size])
 {
@@ -14,50 +14,49 @@ int printArr(int arr[Size][Size])
 }
 int main(void)
 {
-    // int x = 0,
-    //     y = 0,
-    //     sCount = 0,
-    //     arr[N][N] = {0};
-
-    // for (int dyStep = 0; dyStep < N; dyStep++)
-    // {
-    //     y = 0;
-    //     x = dyStep;
-    //     for (int dxStep = 0; dxStep < dyStep + 1; dxStep++)
-    //     {
-    //         arr[y][x] = ++sCount;
-    //         y++;
-    //         x--;
-    //     }
-    // }
     int arr[Size][Size] = {0};
     int counter = 1;
-    for (int i = 0; i < Size; i++)
+    // fill full matrix
+    // change: the matrix is imaginary extended, filling occurs only if the condition is met
+    for (int i = 0; i < Size * 2; i++)
     {
+        printf("%d\n", i);
         if (i % 2 == 0)
         {
             int m = i;
             int n = 0;
             while (m >= 0)
             {
-                arr[m][n] = counter;
+                if ((m < Size) && (m >= 0) && (n < Size) && (n >= 0))
+                {
+                    arr[m][n] = counter;
+                    counter++;
+                    printf("%d - %d\n", m + 1, n + 1);
+                }
+
                 m--;
                 n++;
-                counter++;
             }
         }
         else
         {
             int m = 0;
             int n = i;
-            while (n >=0)
+            while (n >= 0)
             {
-                arr[m][n] = counter;
+
+                if ((m < Size) && (m >= 0) && (n < Size) && (n >= 0))
+                {
+                    arr[m][n] = counter;
+                    counter++;
+                    printf("%d - %d\n", m + 1, n + 1);
+                }
+
                 m++;
                 n--;
-                counter++;
             }
         }
     }
+    // fill second part
     printArr(arr);
 }
