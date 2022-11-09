b4cf9dbef7c35679e438684e8caaa694b508569c
Turchanov Denis <turchanovdenis786@gmail.com>
2022-11-09 21:08:21 +0200

    condition for 1 var

--- lab2/main.c
+++ lab2/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <errno.h>
 int toNumber(char string[])
 {
     long int number = 0;
@@ -28,19 +28,56 @@ int toNumberModify(char string[])
 
 int calc(char string[])
 {
-   
-    
-}
-int main(void)
-{
-    calc("12*789=");
+    // cheking on mistake
+    if ((!((string[1] == '+') || (string[1] == '-') || (string[1] == '/') || (string[1] == '*')) || (string[3] != '=') || !((string[0] == 'x') || (string[2] == 'x'))))
+    {
+        perror("Incorect expression");
+    }
 
-    
+    if (string[0] == 'x')
+    {
+        // printf("re");
+        switch (string[1])
+        {
+        case '+':
+            return atoi(&string[4]) - atoi(&string[2]);
 
-    // char *expression[] = {"2+x=5", "x+2=5", "2+x=5", "x-5=2", "5-x=2", "x=5*2", "x*5=10", "9*9=x", "x=10/3", "x/2=5", "10/x=5"};
+        case '-':
+            return atoi(&string[4]) + atoi(&string[2]);
+        case '*':
+            return atoi(&string[4]) / atoi(&string[2]);
 
-    // int len = sizeof(expression) / sizeof(char *);
+        case '/':
+            return atoi(&string[4]) * atoi(&string[2]);
+        }
 
-    // for (int i = 0; i < len; i++)
-    //     printf("%s x=%d\n", expression[i], Calc(expression[i]));
-}
+        if (string[2] == 'x')
+        {
+            // printf("re");
+            switch (string[1])
+            {
+            case '+':
+                return atoi(&string[4]) - atoi(&string[2]);
+
+            case '-':
+                return atoi(&string[4]) + atoi(&string[2]);
+            case '*':
+                return atoi(&string[4]) / atoi(&string[2]);
+
+            case '/':
+                return atoi(&string[4]) * atoi(&string[2]);
+            }
+        }
+    }
+    int main(void)
+    {
+        int res = calc("x+1=6");
+        printf("%d", res);
+
+        // char *expression[] = {"2+x=5", "x+2=5", "2+x=5", "x-5=2", "5-x=2", "x=5*2", "x*5=10", "9*9=x", "x=10/3", "x/2=5", "10/x=5"};
+
+        // int len = sizeof(expression) / sizeof(char *);
+
+        // for (int i = 0; i < len; i++)
+        //     printf("%s x=%d\n", expression[i], Calc(expression[i]));
+    }
