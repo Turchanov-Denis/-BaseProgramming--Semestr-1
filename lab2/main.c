5c82e56451dff3ce63ef329f6a947b87b8fa6be9
Turchanov Denis <turchanovdenis786@gmail.com>
2022-11-09 21:24:34 +0200

    simple expression calc

--- lab2/main.c
+++ lab2/main.c
@@ -1,42 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-int toNumber(char string[])
-{
-    long int number = 0;
-    for (int i = 0; string[i] != '\0'; i++)
-    {
-        int x = string[i] - '0';
-        if (x < 0 || x > 9)
-            return 0;
-        number = number * 10 + x;
-    }
-    return number;
-}
-
-int toNumberModify(char string[])
-{
-    long int number = 0;
-    for (int i = 0; string[i] != '\0'; i++)
-    {
-        int x = string[i] - '0';
-        if (x >= 0 && x <= 9)
-            number = number * 10 + x;
-    }
-    return number;
-}
 
 int calc(char string[])
 {
     // cheking on mistake
     if ((!((string[1] == '+') || (string[1] == '-') || (string[1] == '/') || (string[1] == '*')) || (string[3] != '=') || !((string[0] == 'x') || (string[2] == 'x'))))
     {
-        perror("Incorect expression");
+        printf("Incorect expression \n");
+        return;
     }
 
     if (string[0] == 'x')
     {
-        // printf("re");
         switch (string[1])
         {
         case '+':
@@ -50,34 +26,28 @@ int calc(char string[])
         case '/':
             return atoi(&string[4]) * atoi(&string[2]);
         }
-
-        if (string[2] == 'x')
-        {
-            // printf("re");
+    }
+     if (string[2] == 'x') {
             switch (string[1])
             {
             case '+':
-                return atoi(&string[4]) - atoi(&string[2]);
+                return atoi(&string[4]) - atoi(&string[0]);
 
             case '-':
-                return atoi(&string[4]) + atoi(&string[2]);
+                return atoi(&string[0]) - atoi(&string[4]);
             case '*':
-                return atoi(&string[4]) / atoi(&string[2]);
+                return atoi(&string[4]) / atoi(&string[0]);
 
             case '/':
-                return atoi(&string[4]) * atoi(&string[2]);
+                return atoi(&string[4]) * atoi(&string[0]);
             }
         }
-    }
+    
+}
     int main(void)
     {
-        int res = calc("x+1=6");
-        printf("%d", res);
-
-        // char *expression[] = {"2+x=5", "x+2=5", "2+x=5", "x-5=2", "5-x=2", "x=5*2", "x*5=10", "9*9=x", "x=10/3", "x/2=5", "10/x=5"};
-
-        // int len = sizeof(expression) / sizeof(char *);
-
-        // for (int i = 0; i < len; i++)
-        //     printf("%s x=%d\n", expression[i], Calc(expression[i]));
+        char *expression[] = {"2+x=5", "x+2=5", "2+x=5", "x-5=2", "5-x=2", "x=5*2", "x*5=10", "9*9=x", "x=10/3", "x/2=5", "10/x=5"};
+        int len = sizeof(expression) / sizeof(char *);
+        for (int i = 0; i < len; i++)
+            printf("%s x=%d\n", expression[i], calc(expression[i]));
     }
