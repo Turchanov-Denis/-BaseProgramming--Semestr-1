03d047268e651359a79410eaf956fad6bc3e04d2
Turchanov-Denis <96124363+Turchanov-Denis@users.noreply.github.com>
2022-12-06 11:00:01 +0200

    minify switch

--- lab4/main.c
+++ lab4/main.c
@@ -3,7 +3,6 @@
 double polynom(char *expr, double x)
 {
     double count = 0.0;
-
     printf("%s\n", expr);
     for (int i = 0; expr[i] != 0; i++)
     {
@@ -12,172 +11,78 @@ double polynom(char *expr, double x)
             // check space
         case ' ':
             break;
-        case '+':
-            i++;
-            // check coefficient
-            if (expr[i] == 'x')
-            {
-                // dont have
-
-                if (expr[i + 1] == '^')
-                {
-                    i++;
-                    count += pow(x, expr[i] - '0');
-                }
-                // alone x
-                else
-                {
-                    count += x;
-                }
-            }
-            else
-            {
-                // have
-                int coefficient = expr[i] - '0';
-                // if coefficient two-digit number
-                if (((expr[i + 1] - '0') >= 0) && ((expr[i + 1] - '0') <= 9))
-                {
-                    i++;
-                    coefficient = coefficient * 10 + (expr[i] - '0');
-                }
-
-                if (expr[i + 1] == '*') // check on just digital
-                {
-                    i++;
-                    i++;
-                    if (expr[i] == 'x')
-                    {
-
-                        if (expr[i + 1] == '^')
-                        {
-                            i++;
-                            i++;
-                            count += coefficient * pow(x, expr[i] - '0');
-                        }
-                    }
-                }
-                else
-                {
-                    // it is just digital
-                    count += coefficient;
-                    break;
-                }
-            }
-            break;
-        case '-':
-            i++;
-            // check coefficient
-            if (expr[i] == 'x')
-            {
-                // dont have
-
-                if (expr[i + 1] == '^')
-                {
-                    i++;
-                    count -= pow(x, expr[i] - '0');
-                }
-                // alone x
-                else
-                {
-                    count -= x;
-                }
-            }
-            else
-            {
-                // have
-                int coefficient = expr[i] - '0';
-                // if coefficient two-digit number
-                if (((expr[i + 1] - '0') >= 0) && ((expr[i + 1] - '0') <= 9))
-                {
-                    i++;
-                    coefficient = coefficient * 10 + (expr[i] - '0');
-                }
-
-                if (expr[i + 1] == '*') // check on just digital
-                {
-                    i++;
-                    i++;
-                    if (expr[i] == 'x')
-                    {
-
-                        if (expr[i + 1] == '^')
-                        {
-                            i++;
-                            i++;
-                            count -= coefficient * pow(x, expr[i] - '0');
-                        }
-                    }
-                }
-                else
-                {
-                    // it is just digital
-                    count -= coefficient;
-                    break;
-                }
-            }
-            break;
         default:
-            i++;
+            int flag = (expr[i] == '-') ? 0 : 1;
+            if ((expr[i] == '+') || (expr[i] == '-')) {
+                i++;
+            }
             // check coefficient
             if (expr[i] == 'x')
             {
                 // dont have
-
                 if (expr[i + 1] == '^')
                 {
-                    i++;
-                    count += pow(x, expr[i] - '0');
-                }
-                // alone x
-                else
-                {
-                    count += x;
+                    i+=2;
+                    if (flag) {
+                        count += pow(x, expr[i] - '0');
+                    }
+                    else {
+                        count -= pow(x, expr[i] - '0');
+                    }
+                } // alone x
+                else {
+                    count = (flag) ? count+x: count-x;
+                    if (flag) {
+                        count += x;
+                    }
+                    else {
+                        count -= x;
+                    }
                 }
             }
             else
             {
-                // have
-                int coefficient = expr[i] - '0';
-                // if coefficient two-digit number
+                int coefficient = expr[i] - '0'; // if coefficient two-digit number
                 if (((expr[i + 1] - '0') >= 0) && ((expr[i + 1] - '0') <= 9))
                 {
                     i++;
                     coefficient = coefficient * 10 + (expr[i] - '0');
                 }
-
                 if (expr[i + 1] == '*') // check on just digital
                 {
-                    i++;
-                    i++;
-                    if (expr[i] == 'x')
-                    {
-
+                    i+=2;
+                    if (expr[i] == 'x'){
                         if (expr[i + 1] == '^')
                         {
-                            i++;
-                            i++;
-                            count += coefficient * pow(x, expr[i] - '0');
+                            i+=2;
+                            if (flag) {
+                                count += coefficient * pow(x, expr[i] - '0');
+                            }
+                            else {
+                                count -= coefficient * pow(x, expr[i] - '0');
+                            }
                         }
                     }
                 }
-                else
-                {
-                    // it is just digital
-                    count += coefficient;
+                else { // it is just digital
+                    if (flag) {
+                        count += coefficient;
+                    }
+                    else {
+                        count -= coefficient;
+                    }
                     break;
                 }
             }
             break;
         }
     }
-
-    // printf("%f", count);
     return count;
 }
 
 int main()
 {
-    char *expr = "-14+10*x^3+x";
+    char *expr = "x+x";
     double x = 3.0;
     printf(" y(% f) = % s returns % f ", x, expr, polynom(expr, x));
 }
