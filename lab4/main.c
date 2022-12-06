2366c06b2c579bc4fc36cee315aeb38cec1e1eab
Turchanov-Denis <96124363+Turchanov-Denis@users.noreply.github.com>
2022-12-06 11:08:34 +0200

    hotfix

--- lab4/main.c
+++ lab4/main.c
@@ -4,12 +4,10 @@ double polynom(char *expr, double x)
 {
     double count = 0.0;
     printf("%s\n", expr);
-    for (int i = 0; expr[i] != 0; i++)
-    {
+    for (int i = 0; expr[i] != 0; i++){
         switch (expr[i])
         {
-            // check space
-        case ' ':
+        case ' ': // check space
             break;
         default:
             int flag = (expr[i] == '-') ? 0 : 1;
@@ -17,61 +15,33 @@ double polynom(char *expr, double x)
                 i++;
             }
             // check coefficient
-            if (expr[i] == 'x')
-            {
-                // dont have
-                if (expr[i + 1] == '^')
-                {
+            if (expr[i] == 'x'){
+                if (expr[i + 1] == '^'){
                     i+=2;
-                    if (flag) {
-                        count += pow(x, expr[i] - '0');
-                    }
-                    else {
-                        count -= pow(x, expr[i] - '0');
-                    }
+                    count = (flag) ? count+pow(x, expr[i] - '0') : count-pow(x, expr[i] - '0');
                 } // alone x
                 else {
                     count = (flag) ? count+x: count-x;
-                    if (flag) {
-                        count += x;
-                    }
-                    else {
-                        count -= x;
-                    }
                 }
             }
-            else
-            {
+            else {
                 int coefficient = expr[i] - '0'; // if coefficient two-digit number
-                if (((expr[i + 1] - '0') >= 0) && ((expr[i + 1] - '0') <= 9))
-                {
+                if (((expr[i + 1] - '0') >= 0) && ((expr[i + 1] - '0') <= 9)){
                     i++;
                     coefficient = coefficient * 10 + (expr[i] - '0');
                 }
-                if (expr[i + 1] == '*') // check on just digital
-                {
+                if (expr[i + 1] == '*') { // check on just digital
                     i+=2;
                     if (expr[i] == 'x'){
                         if (expr[i + 1] == '^')
                         {
                             i+=2;
-                            if (flag) {
-                                count += coefficient * pow(x, expr[i] - '0');
-                            }
-                            else {
-                                count -= coefficient * pow(x, expr[i] - '0');
-                            }
+                            count = (flag) ? count+coefficient * pow(x, expr[i] - '0'):count-coefficient * pow(x, expr[i] - '0');
                         }
                     }
                 }
                 else { // it is just digital
-                    if (flag) {
-                        count += coefficient;
-                    }
-                    else {
-                        count -= coefficient;
-                    }
-                    break;
+                    count= (flag) ? count+coefficient:count-coefficient;
                 }
             }
             break;
@@ -79,10 +49,8 @@ double polynom(char *expr, double x)
     }
     return count;
 }
-
-int main()
-{
-    char *expr = "x+x";
+int main(){
+    char *expr = "x^4+15*x^3-3*x^2+x-2";
     double x = 3.0;
     printf(" y(% f) = % s returns % f ", x, expr, polynom(expr, x));
 }
